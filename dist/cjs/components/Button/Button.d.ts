400aa41976baa91e087d1c09c411e4e47d395c06
mazusu <qweghj1245@gmail.com>
2023-03-07 17:03:52 +0800

    test

--- dist/cjs/components/Button/Button.d.ts
+++
@@ -1,6 +0,0 @@
-type Props = {
-    title: string;
-    onClick: () => void;
-};
-declare const Button: (props: Props) => JSX.Element;
-export default Button;
--- dist/esm/components/Button/Button.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const Button = (props) => {
-    return (React.createElement("button", { onClick: props.onClick }, props.title));
-};
-export default Button;
-//# sourceMappingURL=Button.js.map
